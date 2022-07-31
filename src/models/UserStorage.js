3e9b4d9c4c5e6836ae2b552272a7a6382a05d112
94lfnv <94lfnv@gmail.com>
2022-07-31 22:58:37 +0900

    fs 모듈을 통한 회원가입 기능 구현

--- src/models/UserStorage.js
+++ src/models/UserStorage.js
@@ -1,14 +1,22 @@
 "use strict";
 
-class UserStorage {
-  static #users = {
-    id: ["somda", "안녕", "이팀장"],
-    psword: ["1234", "1234", "12345"],
-    name: ["솜다리", "다리솜", "리다솜"],
-  };
+const fs = require("fs").promises;
 
-  static getUsers(...fields) {
-    const users = this.#users;
+class UserStorage {
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
+  }
+
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) return users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -18,22 +26,33 @@ class UserStorage {
     return newUsers;
   }
 
-  static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const userInfo = Object.keys(users).reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-
-    return userInfo;
+  static getUsers(isAll, ...fields) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUsers(data, isAll, fields);
+      })
+      .catch(console.err);
   }
 
-  static save(userInfo) {
-    const users = this.#users;
+  static getUserInfo(id) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.err);
+  }
+
+  static async save(userInfo) {
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      throw "이미 존재하는 아이디입니다.";
+    }
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
     return { success: true };
   }
 }
