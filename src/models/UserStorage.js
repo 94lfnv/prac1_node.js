e89e42a69b60ac391534a1bea7f012ca344be065
94lfnv <94lfnv@gmail.com>
2022-07-31 18:58:09 +0900

    node.js first commit

---
+++ src/models/UserStorage.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class UserStorage {
+  static #users = {
+    id: ["somda", "안녕", "이팀장"],
+    psword: ["1234", "1234", "12345"],
+    name: ["솜다리", "다리솜", "리다솜"],
+  };
+
+  static getUsers(...fields) {
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const userInfo = Object.keys(users).reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
+}
+
+module.exports = UserStorage;
---
+++ src/public/js/home/login.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  // 앞서 요청한 아이디와 비밀번호를 서버와 연결해주려면?
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.err("로그인 중 에러가 발생했습니다.");
+    });
+}
