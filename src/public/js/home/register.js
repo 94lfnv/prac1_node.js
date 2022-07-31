afc03a8b48b165d817d7753d02a91962991fd91f
94lfnv <94lfnv@gmail.com>
2022-07-31 21:20:32 +0900

    delete folder

--- src/public/js/home/register.js
+++ src/public/js/home/register.js
@@ -9,11 +9,14 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (psword.value !== confirmPsword.value)
+    return alert("비밀번호가 일치하지 않습니다.");
+
   const req = {
     id: id.value,
     name: name.value,
     psword: psword.value,
-    confirmPsword: confirmPsword.value,
   };
 
   // 앞서 요청한 아이디와 비밀번호를 서버와 연결해주려면?
